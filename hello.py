a5fdd4c75949bb0e44cd90dbb090594929bb78b2
v123582 <v123582@gmail.com>
2014-12-30 23:29:43 +0800

    modify hello.py

--- hello.py
+++ hello.py
@@ -11,5 +11,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
