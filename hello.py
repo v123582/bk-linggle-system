7bf825bd0b38c86b66fd15f81af260b4e97d0a5e
v123582 <v123582@gmail.com>
2014-12-30 23:45:32 +0800

    upgrade

--- hello.py
+++ hello.py
@@ -31,4 +31,4 @@ def ajax_post_test():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
