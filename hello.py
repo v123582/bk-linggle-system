ae4e44ebe9ccb63be56e1b16f611fed6774597ab
v123582 <v123582@gmail.com>
2014-12-31 01:53:54 +0800

    test

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
 import os
 from flask import Flask, render_template, request, url_for
+from flask.ext.shelve import get_shelve, init_app
+
 app = Flask(__name__)
+app.config['SHELVE_FILENAME'] = 'word_result.shelve'
+init_app(app)
+word_complete =  get_shelve('c')
+
+#word_complete = shelve.open('static/word_result.shelve' , 'r')
 
 @app.route('/')
 @app.route('/data')
@@ -13,7 +20,7 @@ def form(name=None):
 	return render_template('test.html',  name='')
 
 def word_complete(word):
-	return ['1','2','3']
+    return word_complete[word]
 
 @app.route("/ajax_post_test", methods=['POST'])
 def ajax_post_test():
