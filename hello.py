d7a67d76ce39bb609c3495302dc2435321f09cf8
v123582 <v123582@gmail.com>
2014-12-30 23:33:31 +0800

    modify hello.py

--- hello.py
+++ hello.py
@@ -11,5 +11,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
