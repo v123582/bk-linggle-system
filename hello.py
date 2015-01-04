f072a38a25ec3229425809d2b68907819844ce6a
v123582 <v123582@gmail.com>
2015-01-04 20:33:32 +0800

    running

--- hello.py
+++ hello.py
@@ -1,22 +1,106 @@
 import os
 from flask import Flask, render_template, request, url_for
+from pattern.en import tag
 import sqlite3dbm
+import itertools
+import re
 
 app = Flask(__name__)
 #app.config['SHELVE_FILENAME'] = 'word_result.shelve'
 #init_app(app)
 #word_complete =  get_shelve('c')
+tag_table = {'adj.':'JJ' , 'det.':'DT' , 'n.':'NN' , 'v.':'VB.*' , 'prep.':'IN'}
 
+
+
+def searchfun(lines):
+    database = sqlite3dbm.sshelve.open('query_result.db')
+    return_list = []
+    try:                
+        for line in database[lines]:
+            return_list.append(line)
+    except Exception, e:
+        pass
+    return return_list
+
+def search_tag(pos_tag , content):
+    tag_index = content.split(" ").index(pos_tag)
+    temp = content.replace(pos_tag , '_')
+    result = searchfun(temp)
+    return_list = []
+    for item in result:
+        pos = str(tag(item)[tag_index][1])
+        pattern = re.compile(tag_table[pos_tag])
+        if pattern.match(pos):
+            return_list.append(item)
+    return return_list 
+def re_align(lists):
+    database = sqlite3dbm.sshelve.open('query_result.db')
+    result = []
+    for statments in list(itertools.permutations(lists , len(lists))):
+        statment =  " ".join(statments)
+        try:
+            if database[statment]:
+                result.append(statment)
+        except Exception, e:
+            pass
+    return result
 
 @app.route('/')
 @app.route('/data')
 def form(name=None):
-	if not request.args.get('k'):
-		return render_template('test.html',  name='')
-	result = str(request.args.get('k'))
-	if result:
-		return render_template('test.html',  name=result)
-	return render_template('test.html',  name='')
+    if not request.args.get('k'):
+        return render_template('test.html',  name='')
+    lines = str(request.args.get('k'))
+    result = []
+    if '|' in lines:
+        lists = lines.split(" ")
+        or_index = lists.index('|')
+        replace_content = lists[or_index - 1]+' | '+lists[or_index + 1]
+        temp = lines.replace(replace_content , lists[or_index - 1])
+        result.extend(searchfun(temp))
+        temp = lines.replace(replace_content , lists[or_index + 1])
+        result.extend(searchfun(temp))
+        return render_template('test.html',  name=result, query = lines)
+    if '_' in lines:
+        result.extend(searchfun(lines))
+        return render_template('test.html',  name=result, query = lines)
+    if '*' in lines:
+        temp = lines.replace('*','_')
+        result.extend(searchfun(temp))
+        temp = lines.replace('*','_ _')
+        result.extend(searchfun(temp))
+        temp = lines.replace('*','_ _ _')
+        result.extend(searchfun(temp))
+        return render_template('test.html',  name=result, query = lines)
+    if '?' in lines:
+        temp = lines.replace('?','')
+        ans = [['False'], ['True']]
+        if not searchfun(temp):
+            return render_template('test.html',  name=ans[0], query = lines)
+        else:
+            return render_template('test.html',  name=ans[1], query = lines)
+    if 'adj.' in lines:
+        result = search_tag('adj.' , lines)
+        return render_template('test.html',  name=result, query = lines)
+    if 'det.' in lines:
+        result = search_tag('det.' , lines)
+        return render_template('test.html',  name=result, query = lines)
+    if 'n.' in lines:
+        result = search_tag('n.' , lines)
+        return render_template('test.html',  name=result, query = lines)
+    if 'v.' in lines:
+        result = search_tag('v.' , lines)
+        return render_template('test.html',  name=result, query = lines)
+    if 'prep.' in lines:
+        result = search_tag('prep.' , lines)
+        return render_template('test.html',  name=result, query = lines)
+    if 'r.' in lines:
+        temp = lines.replace('r. ','')
+        lists = temp.split(" ")
+        result = re_align(lists)
+        return render_template('test.html', name=result, query = lines)
+    return render_template('test.html',  name='')
 
 def word_complete(word):
     word_completex = sqlite3dbm.sshelve.open('word_result.db')
@@ -39,6 +123,7 @@ def ajax_post_test():
 	    hint_list += '<li><a href="">'+word+'</a></li>'
     result = '<ul>'+ hint_list + '</ul>'
     return result
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
