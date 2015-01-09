41fdb079cbad84f7feb1703d26dd81242fe6a638
v123582 <v123582@gmail.com>
2015-01-09 08:16:13 +0800

    final

--- hello.py
+++ hello.py
@@ -14,7 +14,7 @@ tag_table = {'adj.':'JJ' , 'det.':'DT' , 'n.':'NN' , 'v.':'VB.*' , 'prep.':'IN'}
 
 
 def searchfun(lines):
-    database = sqlite3dbm.sshelve.open('/Users/mac/Desktop/query_result.db')
+    database = sqlite3dbm.sshelve.open('query_result.db')
     return_list = []
     try:
         for line in database[lines]:
@@ -35,13 +35,13 @@ def search_tag(pos_tag , content):
             return_list.append(item)
     return return_list
 def re_align(lists):
-    database = sqlite3dbm.sshelve.open('/Users/mac/Desktop/query_result.db')
+    database = sqlite3dbm.sshelve.open('query_result.db')
     result = []
     for statments in list(itertools.permutations(lists , len(lists))):
         statment =  " ".join(statments)
         try:
             if database[statment]:
-                result.append(statment)
+                result.append(database[statment][0])
         except Exception, e:
             pass
     return result
@@ -52,10 +52,25 @@ def getkey(result):
 @app.route('/')
 @app.route('/data')
 def form(name=None):
+    if not request.args.get('index'):
+        index = '1'
+    else:
+        index = str(request.args.get('index'))
     if not request.args.get('k'):
         return render_template('test.html',  name='')
     lines = str(request.args.get('k'))
     result = []
+    if '?' in lines:
+        temp = lines.replace('?','')
+        ans = [['False'], ['True']]
+        if not searchfun(temp):
+            return render_template('test.html',  name=ans[0], query = lines)
+        else:
+            return render_template('test.html',  name=ans[1], query = lines)
+    
+
+
+
     if '|' in lines:
         lists = lines.split(" ")
         or_index = lists.index('|')
@@ -65,10 +80,8 @@ def form(name=None):
         temp = lines.replace(replace_content , lists[or_index + 1])
         result.extend(searchfun(temp))
         result = sorted(result , key = getkey , reverse=True)
-        return render_template('test.html',  name=result, query = lines)
     if '_' in lines:
         result.extend(searchfun(lines))
-        return render_template('test.html',  name=result, query = lines)
     if '*' in lines:
         temp = lines.replace('*','_')
         result.extend(searchfun(temp))
@@ -77,41 +90,30 @@ def form(name=None):
         temp = lines.replace('*','_ _ _')
         result.extend(searchfun(temp))
         result = sorted(result , key = getkey , reverse=True)
-        return render_template('test.html',  name=result, query = lines)
-    if '?' in lines:
-        temp = lines.replace('?','')
-        ans = [['False'], ['True']]
-        if not searchfun(temp):
-            return render_template('test.html',  name=ans[0], query = lines)
-        else:
-            return render_template('test.html',  name=ans[1], query = lines)
     if 'adj.' in lines:
         result = search_tag('adj.' , lines)
-        return render_template('test.html',  name=result, query = lines)
     if 'det.' in lines:
         result = search_tag('det.' , lines)
-        return render_template('test.html',  name=result, query = lines)
     if 'n.' in lines:
         result = search_tag('n.' , lines)
-        return render_template('test.html',  name=result, query = lines)
     if 'v.' in lines:
         result = search_tag('v.' , lines)
-        return render_template('test.html',  name=result, query = lines)
     if 'prep.' in lines:
         result = search_tag('prep.' , lines)
-        return render_template('test.html',  name=result, query = lines)
     if 'r.' in lines:
         temp = lines.replace('r. ','')
         lists = temp.split(" ")
         result = re_align(lists)
-        return render_template('test.html', name=result, query = lines)
-    database = sqlite3dbm.sshelve.open('/Users/mac/Desktop/query_result.db')
+    size = min(len(result),76)
+    if size != 0:
+        return render_template('test.html',index=index, name=result, query = lines, size=size)
+
+    database = sqlite3dbm.sshelve.open('query_result.db')
     try:
         result = database[lines]
     except Exception, e:
         result = ["Not Found!!"]
-    
-    return render_template('test.html',   name=result, query = lines)
+    return render_template('test.html',index=index, name=result, query = lines, size=size)
 
 
 def word_complete(lines):
@@ -153,7 +155,7 @@ def ajax_post_test():
     words = word_complete(request.form["value"])
     for word in words:
         word = ' '.join(word.split(' ')[:-1])
-        hint_list += '<li><a href="?k='+ word +'">'+word+'</a></li>'
+        hint_list += '<li><a href="?index=1&k='+ word +'">'+word+'</a></li>'
     result = '<ul>'+ hint_list + '</ul>'
     return result
 
