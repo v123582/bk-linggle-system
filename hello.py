641019d8c9d4840d9205255cc6c245dd3108fd1f
v123582 <v123582@gmail.com>
2015-01-14 05:07:28 +0800

    running

--- hello.py
+++ hello.py
@@ -4,14 +4,12 @@ from pattern.en import tag
 import sqlite3dbm
 import itertools
 import re
-
+from math import fabs
 app = Flask(__name__)
 #app.config['SHELVE_FILENAME'] = 'word_result.shelve'
 #init_app(app)
 #word_complete =  get_shelve('c')
 tag_table = {'adj.':'JJ' , 'det.':'DT' , 'n.':'NN' , 'v.':'VB.*' , 'prep.':'IN'}
-
-
 
 def searchfun(lines):
     database = sqlite3dbm.sshelve.open('query_result.db')
@@ -22,6 +20,54 @@ def searchfun(lines):
     except Exception, e:
         pass
     return return_list
+
+def search_all_index(lists , target):
+    result = []
+    for index ,word in enumerate(lists):
+        if target == word:
+            result.append(index)
+    return result
+
+def gdex(lists):
+    dicts = sqlite3dbm.sshelve.open("gdex.db" , "r")
+    Sent = sqlite3dbm.sshelve.open("Sents.db" , "r")
+    result = []
+    lists = lists.split(" ")[:-1]
+    fin_set = set()
+    flag = 1
+    try:
+        for word in lists:
+            if flag:
+                for sent_index in dicts[word]:
+                    fin_set.add(sent_index)
+                flag = 0
+            else:
+                temp_set = set()
+                for sent_index in dicts[word]:
+                    temp_set.add(sent_index)
+                fin_set = fin_set & temp_set
+        for sent_index in fin_set:
+            Sent_list = Sent[str(sent_index)]
+            begin_index = search_all_index(Sent_list , lists[0])
+            for word in lists[1:]:
+                next_index = search_all_index(Sent_list , word)
+                cum_flag = False
+                for i in begin_index:
+                    for n in next_index:
+                        if(int(fabs(i - n)) < 3):
+                            cum_flag = True
+                            break
+                    if cum_flag:
+                        break
+                if not cum_flag:
+                    break
+                else:
+                    begin_index = search_all_index(Sent_list , word)
+            if cum_flag:
+                return " ".join(Sent_list)
+    except Exception, e:
+        return ""
+    return result 
 
 def search_tag(pos_tag , content):
     tag_index = content.split(" ").index(pos_tag)
@@ -48,6 +94,25 @@ def re_align(lists):
 
 def getkey(result):
     return int(result.split(" ")[-1])
+
+def get_word(lists):
+    return lists.split(" ")[0]
+
+def word_re_align(words):
+    word_complete = sqlite3dbm.sshelve.open('word_result.db' , 'r')
+    result = []
+    for word in itertools.permutations(words , len(words)):
+        word =  "".join(word)
+        try:
+            if(get_word(word_complete[word][0]) == word):
+                result.append(word_complete[word][0])
+        except Exception, e:
+            pass
+    if not result:
+        return ['Not Found!! 0']
+    else:
+        result = sorted(result , key = getkey , reverse=True)
+        return result[0]
 
 @app.route('/')
 @app.route('/data')
@@ -105,15 +170,24 @@ def form(name=None):
         lists = temp.split(" ")
         result = re_align(lists)
     size = min(len(result),76)
-    if size != 0:
-        return render_template('test.html',index=index, name=result, query = lines, size=size)
-
+    words = []
+    numbers = []
+    gdexs = []
     database = sqlite3dbm.sshelve.open('query_result.db')
     try:
         result = database[lines]
     except Exception, e:
         result = ["Not Found!!"]
-    return render_template('test.html',index=index, name=result, query = lines, size=size)
+    for item in result:
+        gdexs.append(gdex(item))
+        item =  item.split(" ")
+        word = " ".join(item[:-1])
+        number = item[-1]
+        words.append(word)
+        numbers.append(number)
+    if size != 0:
+        return render_template('test.html',index=index, name=result,gdex = gdexs, query = lines, size=size)
+    return render_template('test.html',index=index, name=result,gdex = gdexs, query = lines, size=size)
 
 
 def word_complete(lines):
@@ -122,6 +196,11 @@ def word_complete(lines):
         if " " not in lines:
             return word_complete[lines][:10]
         elif " " == lines[-1] and "_" not in lines:
+            word = lines.split(" ")[-2]
+            try:
+                word_complete[word]
+            except Exception, e:
+                return word_re_align(word)
             if not searchfun((lines + "_")):
                 return ['Not Found!! 0']
             else:
@@ -151,12 +230,18 @@ def ajax_post_test():
     #    return "<p>Hello AJAX " + request.form["name"].upper() + " " + request.form["city"].upper() + " Test.</p>";
     hint_list = ''
     if request.form["value"] == '':
-	    return ''
+        return ''
     words = word_complete(request.form["value"])
-    for word in words:
-        word = ' '.join(word.split(' ')[:-1])
-        hint_list += '<li><a href="?index=1&k='+ word +'">'+word+'</a></li>'
-    result = '<ul>'+ hint_list + '</ul>'
+    if type(words) == str:
+        words = words.split(" ")[0]
+        result = 'Do you query "<a href="?index=1&k='+ words +'">'+words+'?</a>'
+    else:
+        for word in words:
+            word = ' '.join(word.split(' ')[:-1])
+            hint_list += '<li><a href="?index=1&k='+ word +'">'+word+'</a></li>'
+        result = '<ul>'+ hint_list + '</ul>'
+
+
     return result
 
 
